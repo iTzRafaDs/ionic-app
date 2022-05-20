c5e617cb39d3743e6f6bd25138f02d802c8cc655
iTzRafaDs <utm19040004@utma.edu.mx>
2022-05-20 08:39:15 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RAFERI',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
